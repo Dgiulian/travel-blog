d8ebf6ef78046347339d6abc678f3f9951961f2c
Diego Giuliani <giuliani.diego@gmail.com>
2020-01-03 00:39:24 -0300

    Fixed thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Layout from "../components/layout"
 import "./contact.css"
-import Nav from '../components/nav'
+import Nav from "../components/nav"
 
 const Thanks = () => {
   return (
     <Layout>
       <Nav />
       <div className="contact__header">
-        <h1>Thank you! I'll be in contact soon.</h1>
+        <div className="contact__thanks">
+          <h1>Thank you! I'll be in contact soon.</h1>
+        </div>
       </div>
     </Layout>
   )
